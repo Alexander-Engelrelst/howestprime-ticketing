e62b021956b6d511ad0b74f05eb2fad5b75eeded
Alexander Engelrelst <alex.engelrelst@gmail.com>
2026-05-12 21:20:26 +0200

    adds the base for the domain layer for now

---
+++ src/Domain/Shared/ValueObjects/ExternalId.ts
@@ -0,0 +1,29 @@
+import { ValueObject } from '@/Domain/Shared/mod.ts';
+
+export class ExternalId extends ValueObject {
+    // The standard UUID pattern: 8-4-4-4-12 hex characters
+    // The [0-9a-f] part covers hex, and 'i' makes it case-insensitive
+    private static readonly UUID_REGEX: RegExp = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
+    private readonly _value: string;
+
+    private constructor(value: string) {
+        super();
+        this._value = value;
+    }
+
+    public static create(value: string): ExternalId {
+        if (!ExternalId.UUID_REGEX.test(value)) {
+            throw new Error(`ExternalId Validation Error: '${value}' is not a valid UUID format.`);
+        }
+        return new ExternalId(value);
+    }
+
+    public get value(): string {
+        return this._value;
+    }
+
+    override equals(other: ExternalId): boolean {
+        return other._value === this._value;
+    }
+}
---
+++ src/Domain/Ticketing/Movies/Movie.ts
@@ -0,0 +1,97 @@
+import { AggregateRoot, ExternalId, Money, UUIDEntityId } from '@/Domain/Shared/mod.ts';
+import { AgeRating, Genre, MovieDuration, MovieTitle, PosterUrl } from '@/Domain/Ticketing/Movies/mod.ts';
+
+
+export class MovieId extends UUIDEntityId {
+    private constructor(id?: string) {
+        super(id);
+    }
+
+    static create(value?: string): MovieId {
+        return new MovieId(value);
+    }
+}
+
+export class Movie extends AggregateRoot<MovieId> {
+    private readonly _title: MovieTitle;
+    private readonly _duration: MovieDuration;
+    private readonly _genres: Genre[];
+    private readonly _ageRating: AgeRating;
+    private readonly _posterUrl: PosterUrl;
+    private readonly _price: Money;
+    private readonly _externalId: ExternalId;
+
+    private constructor(
+        id: MovieId,
+        title: MovieTitle,
+        duration: MovieDuration,
+        genres: Genre[],
+        ageRating: AgeRating,
+        posterUrl: PosterUrl,
+        price: Money,
+        externalId: ExternalId
+    ) {
+        super(id);
+        this._title = title;
+        this._duration = duration;
+        this._genres = genres;
+        this._ageRating = ageRating;
+        this._posterUrl = posterUrl;
+        this._price = price;
+        this._externalId = externalId;
+    } 
+
+    static create(
+        title: string,
+        duration: number,
+        genres: string[],
+        ageRating: number,
+        posterUrl: string,
+        price: number,
+        externalId: ExternalId
+    ): Movie {
+        return new Movie(
+            MovieId.create(),
+            MovieTitle.create(title),
+            MovieDuration.create(duration),
+            genres.map(Genre.create),
+            AgeRating.create(ageRating),
+            PosterUrl.create(posterUrl),
+            Money.create(price),
+            externalId
+        );
+    }
+
+    override get id(): MovieId {
+        return super.id as MovieId;
+    }
+
+    get title(): MovieTitle {
+        return this._title;
+    }
+
+    get duration(): MovieDuration {
+        return this._duration;
+    }
+
+    get genres(): Genre[] {
+        // genres are valueobject and thus immutable, we can use shallow copy TODO: ask about this
+        return [...this._genres];
+    }
+
+    get ageRating(): AgeRating {
+        return this._ageRating;
+    }
+
+    get posterUrl(): PosterUrl {
+        return this._posterUrl;
+    }
+
+    get price(): Money {
+        return this._price;
+    }
+
+    get externalId(): ExternalId {
+        return this._externalId;
+    }
+}
