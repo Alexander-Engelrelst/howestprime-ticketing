b694ac33e1fa348811a80f79ca752ac3d4452c09
Alexander Engelrelst <alex.engelrelst@gmail.com>
2026-05-14 00:38:31 +0200

    refactors the document mapper for movies

--- src/Infrastructure/Persistence/MongoDb/Repositories/Mappers/MovieDocumentMapper.ts
+++ src/Infrastructure/Persistence/MongoDb/Repositories/Mappers/MovieDocumentMapper.ts
@@ -6,11 +6,7 @@ import { Money } from '@/Domain/Shared/mod.ts';
 
 export class MovieDocumentMapper implements DocumentMapper<Movie> {
     toDocument(movie: Movie): Document {
-        const movieWithVersion = movie as unknown as { __v: number };
-
-        // TODO(alexander): if time remains add incrementing version number,
-        // remove upsert functionality and add retry logic/policy in the application layer
-        const document = serializeObjectToDocument({
+      const document = serializeObjectToDocument({
             _id: movie.id.toString(),
             title: movie.title.value,
             duration: movie.duration.value,
@@ -18,48 +14,21 @@ export class MovieDocumentMapper implements DocumentMapper<Movie> {
             ageRating: movie.ageRating.value,
             posterUrl: movie.posterUrl.value,
             price: movie.price.value,
-            __v: movieWithVersion.__v || 0,
         });
         return document;
     }
 
     reconstitute(document: Document): Movie {
-        const movie = this.reconsituteFromDocument(
-            document._id,
-            document.title,
-            document.duration,
-            document.genres,
-            document.ageRating,
-            document.posterUrl,
-            document.price,
-        );
-
-        if (document.__v !== undefined) {
-            (movie as unknown as { __v: number }).__v = document.__v;
-        }
-
-        return movie;
-    }
-
-    private reconsituteFromDocument(
-        id: string,
-        title: string,
-        duration: number,
-        genres: string[],
-        ageRating: number,
-        posterUrl: string,
-        price: number,
-    ): Movie {
         const movie = Object.create(Movie.prototype);
-        movie['_id'] = MovieId.create(id);
-        movie['_title'] = title;
-        movie['_duration'] = duration;
-        movie['_genres'] = genres.map(Genre.create);
-        movie['_ageRating'] = AgeRating.create(ageRating);
-        movie['_posterUrl'] = PosterUrl.create(posterUrl);
-        movie['_price'] = Money.create(price);
-        movie['__v'] = 0; // will be set by the reconsitute method
+        movie['_id'] = MovieId.create(document._id);
+        movie['_title'] = document.title;
+        movie['_duration'] = document.duration;
+        movie['_genres'] = document.genres.map(Genre.create);
+        movie['_ageRating'] = AgeRating.create(document.ageRating);
+        movie['_posterUrl'] = PosterUrl.create(document.posterUrl);
+        movie['_price'] = Money.create(document.price);
 
         return movie as Movie;
     }
+    
 }
