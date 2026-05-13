185a2fc0a73f8183237d4d037d2f87b212892ab3
Alexander Engelrelst <alex.engelrelst@gmail.com>
2026-05-14 01:31:10 +0200

    adds the repo implementation for orders

---
+++ src/Infrastructure/Persistence/MongoDb/Repositories/MongoDbOrderRepository.ts
@@ -0,0 +1,19 @@
+import { MongoDbClient, MongoDbRepository } from '@/Infrastructure/Persistence/MongoDb/Shared/mod.ts';
+import { Order, OrderId, OrderRepository } from '@/Domain/Ticketing/Orders/mod.ts';
+import { OrderDocumentMapper } from '@/Infrastructure/Persistence/MongoDb/Repositories/mod.ts';
+
+export class MongoDbOrderRepository extends MongoDbRepository<Order, OrderId>
+    implements OrderRepository {
+    static override readonly collectionName: string = 'orders';
+
+    constructor(
+        client: MongoDbClient,
+        mapper: OrderDocumentMapper,
+    ) {
+        super(client, mapper, MongoDbOrderRepository.collectionName);
+    }
+
+    override get entityName(): string {
+        return Order.name;
+    }
+}
--- src/Infrastructure/Persistence/MongoDb/Repositories/mod.ts
+++ src/Infrastructure/Persistence/MongoDb/Repositories/mod.ts
@@ -2,3 +2,5 @@ export { SuggestionDocumentMapper } from './Mappers/SuggestionDocumentMapper.ts'
 export { MongoDbSuggestionRepository } from './MongoDbSuggestionRepository.ts';
 export { MongoDbMovieRepository } from './MongoDbMovieRepository.ts';
 export { MovieDocumentMapper } from './Mappers/MovieDocumentMapper.ts';
+export { MongoDbOrderRepository } from './MongoDbOrderRepository.ts';
+export { OrderDocumentMapper } from './Mappers/OrderDocumentMapper.ts';
