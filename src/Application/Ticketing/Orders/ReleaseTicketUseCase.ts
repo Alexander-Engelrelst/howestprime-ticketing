8f7ccb583b9c4f6069a01fb279edd2581ff8406b
Alexander Engelrelst <alex.engelrelst@gmail.com>
2026-05-20 00:53:39 +0200

    adds the usecase to release tickets

---
+++ src/Application/Ticketing/Orders/ReleaseTicketUseCase.ts
@@ -0,0 +1,42 @@
+import { Order, OrderId, OrderRepository } from '@/Domain/Ticketing/Orders/mod.ts';
+import { Logger, UnitOfWork, UseCase } from '@/Application/Ports/mod.ts';
+
+export interface ReleaseTicketUseCaseInput {
+    orderId: string;
+}
+
+export class ReleaseTicketUseCase implements UseCase<ReleaseTicketUseCaseInput, void> {
+    constructor(
+        private readonly _unitOfWork: UnitOfWork,
+        private readonly _logger: Logger,
+    ) {}
+
+    async execute(input: ReleaseTicketUseCaseInput): Promise<void> {
+        this._logger.debug('Releasing ticket', {
+                orderId: input.orderId,
+        });
+
+        await this._unitOfWork.do(async () => {
+            const orderRepository = this._unitOfWork.getRepository<OrderRepository>(Order.name);
+            const orderOpt = await orderRepository.byId(OrderId.create(input.orderId));
+
+            if (!orderOpt.isPresent) {
+                this._logger.error('Order not found', {
+                    orderId: input.orderId,
+                });
+
+                return;
+            }
+
+            const order = orderOpt.value;
+
+            // for simplicity no rollback is implemented if releasing fails
+            order.releaseTickets();
+            await this._unitOfWork.save(order);
+
+            this._logger.info('Ticket released', {
+                orderId: input.orderId,
+            });
+        });
+    }
+}
