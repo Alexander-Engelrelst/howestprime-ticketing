f58bbfa5c665db7c1cf7f1225702de1a3976335f
Alexander Engelrelst <alex.engelrelst@gmail.com>
2026-05-19 15:14:01 +0200

    adds domain tests

--- src/Application/Policies.ts
+++ src/Application/Policies.ts
@@ -1,9 +1,15 @@
 import { WhenSuggestionCreatedConsoleLogSuggestion } from '@/Application/Ticketing/Suggestions/mod.ts';
+import { WhenPaymentSucceededThenMarkOrderAsPaid } from '@/Application/Ticketing/Orders/WhenPaymentSucceededThenMarkOrderAsPaid.ts';
 import { SuggestionCreatedDomainEvent } from '@/Domain/Ticketing/Suggestions/mod.ts';
+import { PaymentSucceededDomainEvent } from '@/Domain/Ticketing/Payments/mod.ts';
 
 export const domainEventPolicies: Map<string, string[]> = new Map([
     [
         SuggestionCreatedDomainEvent.FQDN_VALUE.toString(),
         [WhenSuggestionCreatedConsoleLogSuggestion.name],
+    ],
+    [
+        PaymentSucceededDomainEvent.FQDN_VALUE.toString(),
+        [WhenPaymentSucceededThenMarkOrderAsPaid.name],
     ],
 ]);
