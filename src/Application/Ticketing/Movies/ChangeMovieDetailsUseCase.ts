a59f19c6c9c231b2e96a60882856d48a453ab3d6
Alexander Engelrelst <alex.engelrelst@gmail.com>
2026-05-20 21:18:29 +0200

    adds the application and domain logic for modifying movie details

---
+++ src/Application/Ticketing/Movies/ChangeMovieDetailsUseCase.ts
@@ -0,0 +1,50 @@
+import { Logger, UnitOfWork, UseCase } from '@/Application/Ports/mod.ts';
+import { AgeRating, Genre, Movie, MovieDuration, MovieId, MovieRepository, PosterUrl } from '@/Domain/Ticketing/Movies/mod.ts';
+import { MovieTitle } from '@/Domain/Ticketing/Movies/ValueObjects/MovieTitle.ts';
+
+export interface ChangeMovieDetailsUseCaseInput {
+    title: string;
+    duration: number;
+    genres: string[];
+    ageRating: number;
+    posterUrl: string;
+    movieId: string;
+}
+
+export class ChangeMovieDetailsUseCase implements UseCase<ChangeMovieDetailsUseCaseInput, void> {
+    constructor(
+        private readonly _unitOfWork: UnitOfWork,
+        private readonly _logger: Logger,
+    ) {}
+
+    async execute(input: ChangeMovieDetailsUseCaseInput): Promise<void> {
+        this._logger.debug('Changing movie details', { input });
+
+        await this._unitOfWork.do(async () => {
+            const movieId = MovieId.create(input.movieId);
+            const movieRepository = await this._unitOfWork.getRepository<MovieRepository>(Movie.name)
+            const movieOpt = await movieRepository.byId(movieId);
+
+            if (!movieOpt.isPresent) {
+                this._logger.warn('Movie not found', { movieId: input.movieId });
+                // for simpilicity rollback procedures are omitted
+                return;
+            }
+
+            const movie = movieOpt.value;
+            movie.changeDetails(
+                MovieTitle.create(input.title),
+                MovieDuration.create(input.duration),
+                input.genres.map((genre) => Genre.create(genre)),
+                AgeRating.create(input.ageRating),
+                PosterUrl.create(input.posterUrl),
+            );
+
+            await this._unitOfWork.save(movie);
+
+            this._logger.info('Movie saved', {
+                movieId: movie.id.value,
+            });
+        });
+    }
+}
